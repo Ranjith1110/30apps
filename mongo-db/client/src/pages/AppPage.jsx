ac5c556bcbeb0a5f95ae7758fdfc0ed0f9bef08b
Ranjith <ranjithram878@gmail.com>
2025-09-16 21:36:15 +0530

    Client & Server Both Done

--- mongo-db/client/src/pages/AppPage.jsx
+++ mongo-db/client/src/pages/AppPage.jsx
@@ -1,14 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Pencil, Trash2, Save, XCircle } from "lucide-react"
+import axios from "axios"
 
 const AppPage = () => {
 
   const [text, setText] = useState("");
-  const [quotes, setQuotes] = useState([
-    { _id: "1", text: "Be Yourself; everyone else is already taken." },
-    { _id: "2", text: "Life is what happen when you're busy making a plans." }
-  ]);
+  const [quotes, setQuotes] = useState([]);
   const [editingId, setEdintingId] = useState(null);
   const [editingText, setEdintingText] = useState("");
+
+  // GET Method
+  const getQuotes = async () => {
+    const res = await axios.get("http://localhost:4000/api/quotes");
+    setQuotes(res.data)
+  }
+
+  // POST Method
+  const handleAdd = async () => {
+
+    if (!text.trim()) {
+      alert("Fill the quotes")
+    } else {
+      await axios.post("http://localhost:4000/api/quotes", { text });
+      setText("");
+      getQuotes();
+    }
+  }
+
+  // PUT Method
+  const startEdit = (id, text) => {
+    setEdintingId(id);
+    setEdintingText(text);
+  }
+
+  // PUT Method
+  const handleUpdate = async () => {
+    if (!editingText.trim()) {
+      alert("Fill the quotes to edit");
+    } else {
+      await axios.put(`http://localhost:4000/api/quotes/${editingId}`, {
+        text:editingText
+      });
+
+      setEdintingId(null);
+      setEdintingText("");
+      getQuotes();
+    }
+
+  }
+
+  // DELETE Method
+  const handleDelete = async (id) => {
+    if (confirm("Are you sure?")) {
+      await axios.delete(`http://localhost:4000/api/quotes/${id}`);
+      getQuotes();
+    }
+  }
+
+  const handleCancel = () => {
+    setEdintingId(null);
+    setEdintingText("");
+  }
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
 
   return (
     <>
@@ -17,17 +73,42 @@ const AppPage = () => {
 
         {/* Create Form */}
         <div className='max-w-xl mx-auto flex gap-4 mb-10'>
-          <input type="text" placeholder='Type your quotes...' className='w-full px-4 py-2 rounded-md border boder-gray-300 shadow-sm' />
-          <button className='bg-[#6f36e8] text-white px-6 py-2 rounded-md hover:bg-[#5c2ec4]'>Add</button>
+          <input type="text" placeholder='Type your quotes...' className='w-full px-4 py-2 rounded-md border boder-gray-300 shadow-sm' onChange={(e) => setText(e.target.value)}
+          value={text} />
+          <button className='bg-[#6f36e8] text-white px-6 py-2 rounded-md hover:bg-[#5c2ec4]' onClick={handleAdd}>Add</button>
         </div>
 
         {/* Quotes List */}
         <div className='max-w-xl mx-auto space-y-4'>
           {quotes.map((q) => (
             <div key={q._id} className='bg-white shadow-md rounded-lg p-4 flex flex-col'>
-              <div className='flex justify-between w-full items-center'>
-                <p>{q.text}</p>
-              </div>
+              {editingId === q._id ? (
+                <div className='flex gap-2'>
+                  <input type="text"
+                    value={editingText}
+                    onChange={(e) => setEdintingText(e.target.value)}
+                    className='w-full px-3 py-2 rounder-md border' />
+
+                  <div className='flex gap-2'>
+                    <button className='bg-green-500 text-white px-3 py-2 rounded-md flex items-center gap-1'
+                      onClick={handleUpdate}>Save</button>
+                    <button className='bg-gray-500 text-white px-3 py-2 rounded-md flex items-center gap-1'
+                      onClick={handleCancel}>Cancel</button>
+                  </div>
+                </div>
+              ) : (
+                <div className='flex justify-between w-full items-center'>
+                  <p>{q.text}</p>
+                  <div className='flex gap-3'>
+                    <button className='text-blue-500 hover:text-blue-800 flex items-center gap-1' onClick={() => startEdit(q._id, q.text)}>
+                      <Pencil size={16} /> Edit
+                    </button>
+                    <button className='text-red-500 hover:text-red-800 flex items-center gap-1' onClick={() => handleDelete(q._id)}>
+                      <Trash2 size={16} /> Delete
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
           ))}
         </div>
--- mongo-db/server/server.js
+++ mongo-db/server/server.js
@@ -6,7 +6,9 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 app.use(express.json());
 
 // Define the schema model
